65b1547f4416c03971f1e437d8db348a7dfb143e
QC3284 <2078725082@qq.com>
2026-08-11 17:52:10 +0800

    version: 2.0.0-go suffix to distinguish Go rewrite

--- cmd/bbdown/main.go
+++ cmd/bbdown/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// Banner: dark blue bg + white text (matching C# Console.BackgroundColor/Console.ForegroundColor)
-	fmt.Print("\033[44m\033[37mBBDown version 2.0.0, Bilibili Downloader.\033[0m\r\n")
+	fmt.Print("\033[44m\033[37mBBDown version 2.0.0-go, Bilibili Downloader.\033[0m\r\n")
 	fmt.Print("遇到问题请首先到以下地址查阅有无相关信息：\r\nhttps://github.com/QC3284/BBDown/issues\r\n")
 	fmt.Println()
 
